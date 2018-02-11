d31d1ddb5d21350e604a1b07692442095b2e40ef
DeArvis Troutman <dtroutma@stetson.edu>
2018-02-11 17:49:40 -0500

    Updated Puzzle Figured Out how to locate the Blank Cell rather than the clicked cell

--- script_index.js
+++ script_index.js
@@ -6,14 +6,13 @@ window.onload = function() {
     const table = document.querySelector('table');   // Selects the Table Class
     const rows = document.querySelectorAll('tr');    // Selects the tr Class
     const rowsArray = Array.from(rows);              // Array from the rows in the table
-// What I need is for the User to Click a square beside or on top of empty square and it moves into the Empty Square
-// Also Need to make a Scramble Button
+
 
     table.addEventListener('click', (event) => {
-        const rowIndex = rowsArray.findIndex(row => row.contains(event.target));
+        const rowIndex = rowsArray.findIndex(row => row.contains(document.getElementById("blank")));
     const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
-    const columnIndex = columns.findIndex(column => column == event.target);
-    console.log(rowIndex, columnIndex)
+    const columnIndex = columns.findIndex(column => column == document.getElementById("blank"));
+    console.log(rowIndex, columnIndex);
     switch_elems(rowIndex, columnIndex);
 })
 }
@@ -23,50 +22,52 @@ function switch_elems(i, j) {
     const table = document.querySelector('table');
     const val1 = table.rows[i].cells[j].innerHTML;
 
-    let k = j + 1;   // J is the value to the right of the clicked area
+    let k = i + 1;   // J is the value to the right of the clicked area
     let numRows = table.rows.length; // not used, but this gets num rows , The answer is 3
 
     if (k > table.rows[i].cells.length - 1) {
         k = 1;
     }
-    const val2 = table.rows[i].cells[k].innerHTML;
+    const val2 = table.rows[k].cells[j].innerHTML;
 
-    table.rows[i].cells[j].innerHTML = val2.toString();
-    table.rows[i].cells[k].innerHTML = val1.toString();
-
-    const rows = document.querySelectorAll('tr');    // Selects the tr Class
-    const rowsArrayBlank = Array.from(rows);
-    const emptySlot = table.getElementsByClassName(' ');
-    const rowIndexBlank = rowsArrayBlank.findIndex(row => row.contains(emptySlot));
-    const columnsBlank = Array.from(rowsArrayBlank[rowIndexBlank].querySelectorAll('td'));
-    const columnIndexBlank = columnsBlank.findIndex(column => column == emptySlot);
-    window.alert("Empty Slot(i,j):" + rowIndexBlank + ", " + columnIndexBlank);
-
-
+    table.rows[i].cells[j].innerHTML = val2.toString(); // Slot You Clicked on (Should sty the same)
+    table.rows[k].cells[j].innerHTML = val1.toString();  // New Slot that will replace slot you clicked
+    window.alert("Blank cell Coordinates: " + i + ", " + j);
 
 }
 
-function valid_click(i,j){
-    // Locate where the empty block is
-        // Make variable empty and find i and j coordinates of it on the grid
-        //
+function valid_click(i,j,v,h){
+    // const table = document.querySelector('table');
+    // const clickedCell = table.rows[i].cells[j].innerHTML;
+    //
+    // const blank = table.rows[v].cells[h].innerHTML;
+    //  blank_x = i - 1;
+    //  blank_y = i + 1;
+    //  clicked_x = i;
+    //  clicked_y = j;
+    // let numRows = table.rows.length;
+//Right of Blank (J=j-1, i=i)
 
+    //Left of Blank
+    //Top of Blank
+    //Bottom of Blank
 
-
-
-    // See if the user clicked on one of the blocks that can swap with the empty
-    // If they did returns true
-    // If they didn't return false
+    // Allow User to click a slot
+    // Check to the right,
+    // the left,
+    // on top,
+    // on bottom
+    // If the blank is on the top or bottom return true, else return false
 }
 
 function puzzle_solver(i,j){
-    const table = document.querySelector('table');   // Selects the Table Class
-    const rows = document.querySelectorAll('tr');    // Selects the tr Class
-    const rowsArray = Array.from(rows);
-    const emptySlot = table.getElementsByClassName(' ');
-    const rowIndex = rowsArray.findIndex(row => row.contains(emptySlot));
-    const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
-    const columnIndex = columns.findIndex(column => column == emptySlot);
+    // const table = document.querySelector('table');   // Selects the Table Class
+    // const rows = document.querySelectorAll('tr');    // Selects the tr Class
+    // const rowsArray = Array.from(rows);
+    // const emptySlot = table.getElementsByClassName(' ');
+    // const rowIndex = rowsArray.findIndex(row => row.contains(emptySlot));
+    // const columns = Array.from(rowsArray[rowIndex].querySelectorAll('td'));
+    // const columnIndex = columns.findIndex(column => column == emptySlot);
     /////////////////////////////////////////////////
 
 
